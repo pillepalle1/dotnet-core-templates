16ac650b99631beee303414cabc1544eea36946c
@pillepalle1 <benj@i9china>
2020-09-21 15:36:05 +0800

    Fixed final bugs in StringTokenizer.cs

--- Pillepalle1.ConsoleTelegramBot/Model/Misc/StringTokenizer.cs
+++ Pillepalle1.ConsoleTelegramBot/Model/Misc/StringTokenizer.cs
@@ -1,14 +1,53 @@
 ﻿using System;
 using System.Text;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 
-namespace Pillepalle1.ConsoleTelegramBot.Model.Misc
+namespace pillepalle1.Text
 {
     public static class StringTokenizer
     {
+        private static FormatException _nonQuotedTokenMayNotContainQuotes =
+            new FormatException("[RFC4180] If fields are not enclosed with double quotes, then double quotes may not appear inside the fields.");
+
+        private static FormatException _quotesMustBeEscapedException =
+            new FormatException("[RFC4180] If double-quotes are used to enclose fields, then a double-quote appearing inside a field must be escaped by preceding it with another double quote.");
+
+        private static FormatException _tokenNotFullyEnclosed =
+            new FormatException("[RFC4180] \"Each field may or may not be enclosed in double quotes\". However, for the final field the closing quotes are missing.");
+
+
         /// <summary>
-        /// Formats and splits the tokens by delimiter allowing to add delimiters by quoting
+        /// <para>
+        /// Formats and splits the tokens by delimiter allowing to add delimiters by quoting 
+        /// similar to https://tools.ietf.org/html/rfc4180
+        /// </para>
+        /// 
+        /// <para>
+        /// Each field may or may not be enclosed in double quotes (however some programs, such as 
+        /// Microsoft Excel, do not use double quotes at all). If fields are not enclosed with 
+        /// double quotes, then double quotes may not appear inside the fields.
+        /// </para>
+        /// 
+        /// <para>
+        /// Fields containing line breaks (CRLF), double quotes, and commas should be enclosed in 
+        /// double-quotes.
+        /// </para>
+        /// 
+        /// <para>
+        /// If double-quotes are used to enclose fields, then a double-quote appearing inside a 
+        /// field must be escaped by preceding it with another double quote.
+        /// </para>
+        /// 
+        /// <para>
+        /// The ABNF defines 
+        /// 
+        /// [field = (escaped / non-escaped)] ||  
+        /// [non-escaped = *TEXTDATA] || 
+        /// [TEXTDATA =  %x20-21 / %x23-2B / %x2D-7E]
+        /// 
+        /// specifically forbidding to include quotes in non-escaped fields, hardening the *SHOULD*
+        /// requirement above.
+        /// </para>
         /// </summary>
         public static ImmutableList<string> SplitRespectingQuotation(this string sourceString, char delimiter = ' ', char quotes = '"')
         {
@@ -37,12 +76,13 @@ namespace Pillepalle1.ConsoleTelegramBot.Model.Misc
                     else if (c == quotes)
                     {
                         tokenBuilder.Append(c);
+                        hasReadTokenChar = true;
                         continue;
                     }
 
                     else
                     {
-                        throw new FormatException($"Unexpected single use of quotes ({quotes}) in quoted token");
+                        throw _quotesMustBeEscapedException;
                     }
                 }
 
@@ -50,28 +90,19 @@ namespace Pillepalle1.ConsoleTelegramBot.Model.Misc
 
                 if (c == quotes)
                 {
-                    // As long as character other than quotes has been added to the token
-                    if (!hasReadTokenChar)
+                    if (isQuoting)
                     {
-                        // If this was the 2n-th token, add a quote-character
-                        if (isQuoting)
-                        {
-                            tokenBuilder.Append(c);
-                        }
-
-                        // We are still about to decide whether we are quoting or not
-                        isQuoting = !isQuoting;
+                        expectingDelimiterOrQuotes = true;
                     }
+
                     else
                     {
-                        if (isQuoting)
+                        if (hasReadTokenChar)
                         {
-                            expectingDelimiterOrQuotes = true;
+                            throw _nonQuotedTokenMayNotContainQuotes;
                         }
-                        else
-                        {
-                            tokenBuilder.Append(c);
-                        }
+
+                        isQuoting = true;
                     }
                 }
 
@@ -80,14 +111,12 @@ namespace Pillepalle1.ConsoleTelegramBot.Model.Misc
                     if (isQuoting)
                     {
                         tokenBuilder.Append(c);
-
                         hasReadTokenChar = true;
                     }
                     else
                     {
                         tokenList = tokenList.Add(tokenBuilder.ToString());
                         tokenBuilder.Clear();
-
                         hasReadTokenChar = false;
                     }
                 }
@@ -96,7 +125,6 @@ namespace Pillepalle1.ConsoleTelegramBot.Model.Misc
                 else
                 {
                     tokenBuilder.Append(c);
-
                     hasReadTokenChar = true;
                 }
             }
@@ -108,7 +136,7 @@ namespace Pillepalle1.ConsoleTelegramBot.Model.Misc
 
             if (isQuoting && !expectingDelimiterOrQuotes)
             {
-                throw new FormatException("Missing closing quotes");
+                throw _tokenNotFullyEnclosed;
             }
 
             return tokenList;
@@ -153,8 +181,11 @@ namespace Pillepalle1.ConsoleTelegramBot.Model.Misc
             }
 
             // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-            // Checking consistency
+            // Tidy up open flags and checking consistency
+            tokenList = tokenList.Add(tokenBuilder.ToString());
+
             if (escapeNext) throw new FormatException();            // Expecting additional char
+
 
             return tokenList;
         }

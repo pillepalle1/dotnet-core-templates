be696cb57138108b7b31a55c1e8f8c91ffb40517
Benj <benj@pillepalle1.de>
2024-06-13 14:10:42 +0800

    Used Verbatim String for DatabaseInitializer

--- Pillepalle1.StatefulApi/src/Customers.Application/Services/Database/SqLite/SqLiteDatabaseInitializer.cs
+++ Pillepalle1.StatefulApi/src/Customers.Application/Services/Database/SqLite/SqLiteDatabaseInitializer.cs
@@ -8,12 +8,14 @@ internal class SqLiteDatabaseInitializer(
         using var dbConnection = await dbConnectionProvider.ProvideAsync();
         using var transaction = dbConnection.BeginTransaction();
         
-        _ = await dbConnection.ExecuteAsync(@"
+        _ = await dbConnection.ExecuteAsync(
+            """
             CREATE TABLE IF NOT EXISTS Customers (
                 Id UUID PRIMARY KEY,
                 Name TEXT NOT NULL,
                 NameNormalized TEXT NOT NULL,
-                Details TEXT NOT NULL);");
+                Details TEXT NOT NULL);
+            """);
             
         transaction.Commit();
     }
